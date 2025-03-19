8b068eee71b6fad67d401e90be2903a243125ab5
AlexWessler <alexizaias09@hotmail.com>
2025-03-18 21:26:25 -0300

    Criacao classe bicicleta

---
+++ Aula00/src/Bicicleta.java
@@ -0,0 +1,13 @@
+public class Bicicleta {
+
+    public String nome;
+
+    public String modelo;
+
+    public Integer marcha;
+
+    public void imprimir() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Modelo: " + modelo);
+    }
+}
---
+++ Aula00/src/TesteBicicleta.java
@@ -0,0 +1,16 @@
+public class TesteBicicleta {
+
+    public static void main (String[] args) {
+        Bicicleta caloi = new Bicicleta();
+        caloi.nome = "Caloi";
+        caloi.marcha = 5;
+        caloi.modelo = "ABC-123";
+
+        Bicicleta k = new Bicicleta();
+        k.nome = "Fast caloi";
+        k.modelo = "FAST";
+
+        caloi.imprimir();
+        k.imprimir();
+    }
+}
